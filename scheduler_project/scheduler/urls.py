2356a73ca38a5a40d9c5d2cc814f25f0b5121fb2
Manish Keshwani <113196913+manishkeshwani@users.noreply.github.com>
2024-11-28 21:22:51 +0530

    Delete scheduler_project directory

--- scheduler_project/scheduler/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('teachers/', views.get_teachers, name='get_teachers'),
-    path('schedules/', views.get_schedules, name='get_schedules'),
-    path('schedules/add/', views.add_schedule, name='add_schedule'),
-]
